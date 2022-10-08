d96a4bb5d02c99e52d3174153f5f77c9751e425e
t4rantino <stephanie.anoruigwe@gmail.com>
2022-10-07 20:04:52 -0700

    Refactor functions

--- scripts/index.js
+++ scripts/index.js
@@ -5,8 +5,8 @@ const roundSelect = document.querySelector('#num-games');
 function incrementScore(e) {
   const player = e.target.attributes['data-type'].value;
   const playerScore = document.querySelector(`span[data-player="${player}"]`);
-  let score = parseInt(playerScore.textContent);
-  playerScore.textContent = `${++score}`;
+  let newScore = parseInt(playerScore.textContent) + 1;
+  playerScore.textContent = `${newScore}`;
 }
 
 function resetScores() {
@@ -29,23 +29,21 @@ function isGameOver() {
 }
 
 function showResults() {
-  const [player1, player2] = scores;
-  let player1Score = parseInt(player1.textContent);
-  let player2Score = parseInt(player2.textContent);
-  let numGames = parseInt(roundSelect.value);
-
-  if (player1Score === numGames) {
-    player1.classList.add('text-accent-100');
-    player2.classList.add('text-accent-500');
-  } else if (player2Score === numGames) {
-    player1.classList.add('text-accent-500');
-    player2.classList.add('text-accent-100');
-  }
+  scores.forEach((score) => {
+    let playerScore = parseInt(score.textContent);
+    let numGames = parseInt(roundSelect.value);
+    if (playerScore === numGames) {
+      score.classList.add('text-accent-100');
+    } else {
+      score.classList.add('text-accent-500');
+    }
+  });
 }
 
 function disableButtons() {
   buttons.forEach((button) => {
-    if (button.attributes['data-type'].value !== 'reset') {
+    let type = button.attributes['data-type'].value;
+    if (type !== 'reset') {
       button.setAttribute('disabled', 'disabled');
     }
   });
@@ -58,8 +56,8 @@ function enableButtons() {
 }
 
 buttons.forEach((button) => {
-  const buttonType = button.attributes['data-type'];
-  if (buttonType.value === 'reset') {
+  let type = button.attributes['data-type'].value;
+  if (type === 'reset') {
     button.addEventListener('click', () => {
       resetScores();
       enableButtons();
